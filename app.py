8148a249232d3c161fab068f3a2cda54e1760f4e
RISHIDIWAKAR197 <jarurihaiisliyebanarhahu@gmail.com>
2026-07-14 11:47:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ st.set_page_config(
     page_title="The Ennoble Trader | Multi-Asset Intraday Tool", 
     page_icon="📊", 
     layout="wide"
+)
 import streamlit as st
 from datetime import datetime, timedelta, timezone
 import pandas as pd
